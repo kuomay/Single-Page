1f2152904ab20329519b05f259a007bd022141df
kuomay <guomei88119@gmail.com>
2024-08-29 10:16:01 +0800

    部屬github page

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,10 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: 'Single-Page',
+  build: {
+    outDir: 'docs',
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
